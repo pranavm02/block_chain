32bab3cc7c8b9d0816ba0c6e2cebe96062c1fb22
Pranav Misal <148197118+pranavm02@users.noreply.github.com>
2024-11-27 21:59:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ const contractABI = [
     }
 ];
 
-const contractAddress = '0x48F2c399A4f736e06deeE1d507530C378A1CD339'; // Replace with the actual contract address from Ganache
+const contractAddress = '0x45Fb5A184eA09c2a8eAa03EA01eEd5c89643c359'; // Replace with the actual contract address from Ganache
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 // Function to hash the file using SHA-256
